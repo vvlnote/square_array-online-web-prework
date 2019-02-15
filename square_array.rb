d02845759c12f10d77c7c9615f959a13416798a6
Joy Lin <vvlnote@hotmail.com>
2019-02-15 01:36:26 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,10 @@
-def square_array(array)
-  newArr = []
-  array.each do |item|
-    newArr.push(item**2)
-  end
-  newArr
-end
+#def square_array(array)
+#  newArr = []
+#  array.each do |item|
+#    newArr.push(item**2)
+#  end
+#  newArr
+#end
 
 def square_array(array)
   newArr = array.collect{|item| item**2}
